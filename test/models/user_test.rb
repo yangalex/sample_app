fa73962e1375358977ee25e3a4c57e8787aaf331
yangalex <yangalex@usc.edu>
2016-04-09 12:18:48 -0700

    Finish log in/log out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -66,7 +66,9 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-
+  test "authenticated? should return false for a user with a nil digest" do
+    assert_not @user.authenticated?('')
+  end
 
 
 
